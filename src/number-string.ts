5223c06d74ab59a9ac8f95ac5d711ef0ad14e52d
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-20 15:23:50 +0900

    lesson13 number型とstring型

---
+++ src/number-string.ts
@@ -0,0 +1,15 @@
+export {};
+
+let year: number = 1976;
+/*
+year = false;
+error TS2322: Type 'boolean' is not assignable to type 'number'.
+*/
+console.log(year);
+
+let age: number = 0x2b;
+
+console.log(age);
+
+let name: string = 'Ham';
+console.log(name);
