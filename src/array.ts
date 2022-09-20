892f16e95093f6febacd8133005e1719c88c8f0b
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-20 19:15:43 +0900

    lesson16 tuple型 #6

--- src/array.ts
+++ src/array.ts
@@ -4,4 +4,13 @@ let numbers: number[] = [1, 2, 3];
 let numbers2: Array<number> = [1, 2, 3];
 let string: Array<string> = ['tokyo', 'osaka', 'saitama'];
 
-let strings: Array<string> = ['Type', 'Java', 'Script'];
+let strings: string[] = ['Type', 'Java', 'Script'];
+let strings2: Array<string> = ['Type', 'Java', 'Script'];
+
+//二次元配列
+let nijigen: number[][] = [
+  [50, 100],
+  [150, 150],
+];
+
+let mix: (string | number | boolean)[] = [1, false, 'japan'];
