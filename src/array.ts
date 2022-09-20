dddedd36a5c1199d8e07497913182bda1fc4c6f2
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-20 16:56:47 +0900

    lesson15 array型 #4

---
+++ src/array.ts
@@ -0,0 +1,7 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+let string: Array<string> = ['tokyo', 'osaka', 'saitama'];
+
+let strings: Array<string> = ['Type', 'Java', 'Script'];
