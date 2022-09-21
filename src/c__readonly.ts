511a68ef5dbedd623cf2c1a1e09a56f111dfdd0f
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-22 08:23:27 +0900

    lesson41 readonly修飾子

---
+++ src/c__readonly.ts
@@ -0,0 +1,21 @@
+export {};
+/*
+class card {
+  readonly owner: string;
+
+  constructor(owner: string) {
+    this.owner = owner;
+  }
+}
+*/
+
+class card {
+  //省略記法。readonlyは変数名の直前！
+  constructor(public readonly owner: string) {}
+}
+
+let visa = new card('Ham');
+console.log(visa.owner);
+
+//visa.owner = 'aaaa';
+//コンパイルエラー：読み取り専用です！
