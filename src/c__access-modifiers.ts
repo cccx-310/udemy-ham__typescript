5ce1fe12bc3601c58c499b5c0f25e15f2ad7a388
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-21 22:00:23 +0900

    lesson38 アクセス修飾子

--- src/c__access-modifiers.ts
+++ src/c__access-modifiers.ts
@@ -1,19 +1,34 @@
 export {};
 
 class Person {
-  name: string;
-  age: number;
+  public name: string; //'public'は基本、省略する
+  private age: number;
+  protected nationality: string;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, nationality: string) {
     this.name = name;
     this.age = age;
+    this.nationality = nationality;
   }
 
   profile(): string {
     return `name: ${this.name}, age: ${this.age}`;
   }
 }
+class Android extends Person {
+  constructor(name: string, age: number, nationality: string) {
+    //親クラスの初期化を流用
+    super(name, age, nationality);
+  }
 
-let taro = new Person('Taro', 30);
+  profile(): string {
+    //ageでコンパイルエラー 親クラスでplivateのため
+    return `name: ${this.name}, age: ${this.age}, nationality: ${this.nationality}`;
+  }
+}
+
+let taro = new Person('Taro', 30, 'JP');
+console.log(taro.profile());
 console.log(taro.name);
+// console.log(taro.age);
 // console.log(taro.profile());
