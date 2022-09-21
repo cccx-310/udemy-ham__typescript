e736699aa071686ed2b9662f8b4a890e8ba86edb
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-21 11:19:09 +0900

    lesson23 interface

--- src/typeAliases.ts
+++ src/typeAliases.ts
@@ -5,7 +5,7 @@ type Mojiretsu = string;
 const fooString: string = 'hello';
 const fooMojiretsu: Mojiretsu = 'hello';
 
-const example = {
+const example1 = {
   name: 'Ham',
   age: 43,
 };
@@ -19,3 +19,8 @@ const example2: Profile = {
   name: 'Ham',
   age: 43,
 };
+
+//example1の構造に合わせて型注釈を設ける
+//example1を参照するので、example1の内容が変われば
+//合わせて型推論→型注釈が自動的に変わる
+type Profile2 = typeof example1;
