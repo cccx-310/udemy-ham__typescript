f371004114661a9cbe30452a16f6b5b3a1ded187
cccx-310 <handbesidestone.sea@gmail.com>
2022-09-21 20:21:09 +0900

    lesson38__access-modifiers

--- src/c__access-modifiers.ts
+++ src/c__access-modifiers.ts
@@ -1,14 +1,10 @@
 export {};
 
 class Person {
-  //コンストラクタメソッドの型注釈はココ！
   name: string;
   age: number;
 
-  //"コンストラクタ"自体には型注釈は不要
-  //例外的で、void型でもない
   constructor(name: string, age: number) {
-    //この中が初期化設定
     this.name = name;
     this.age = age;
   }
@@ -19,4 +15,5 @@ class Person {
 }
 
 let taro = new Person('Taro', 30);
-console.log(taro.profile());
+console.log(taro.name);
+// console.log(taro.profile());
